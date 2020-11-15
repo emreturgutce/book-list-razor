0645d665302769f0cef7a4f162bdb36b2db14389
emreturgutce <emrebursalee@gmail.com>
2020-11-15 14:20:00 +0300

    Create database connection

---
+++ book-list-razor/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_list_razor.Model
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
