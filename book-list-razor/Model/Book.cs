4bd4b5da0baafee0ea2405ef01a99e2e38729a7c
emreturgutce <emrebursalee@gmail.com>
2020-11-15 14:20:11 +0300

    Book model created

---
+++ book-list-razor/Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_list_razor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
