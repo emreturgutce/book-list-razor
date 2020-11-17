250758ed6cfd2236aa99ad9a9dc862c7d1f38752
emreturgutce <emrebursalee@gmail.com>
2020-11-17 14:43:23 +0300

    Migrations for book model ISBN prop

---
+++ book-list-razor/Migrations/20201117113518_AddISBNToBookModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace book_list_razor.Migrations
+{
+    public partial class AddISBNToBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+        }
+    }
+}
